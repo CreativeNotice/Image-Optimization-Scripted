26c6e3945facff9a11a83e9267b4b27aee0294cc
Ryan Mueller <ryanm@pjtrailers.com>
2016-11-10 08:51:34 -0600

    Initial code commit plus readme and license.

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+const gulp     = require( 'gulp' );
+const imagemin = require( 'gulp-imagemin' );
+const webp     = require( 'gulp-webp' );
+
+gulp.task( 'imagemin', () =>
+    gulp.src( '*.{jpg,jpeg,png,gif,svg}' )
+        .pipe( imagemin() )
+        .pipe( gulp.dest( 'dist' ) )
+);
+
+gulp.task( 'webp', [ 'imagemin' ], () =>
+  gulp.src( 'dist/*.{jpg,jpeg,gif}' )
+      .pipe( webp() )
+      .pipe( gulp.dest( 'dist' ) )
+);
+
+gulp.task( 'default', [ 'webp' ] );
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Image-Optimization-Scripted
 Gulp file and dependencies for scripted image optimization.
+
+## Get Started
+When you've a lot of images to optimize and don't want to use an online service or software with a UI...
+
+1. Pull down this repository.
+2. Install dependancies with `npm install`.
+3. Run `gulp`.
+
+You'll end up with a new directory in your working directory named _dist_. Inside that directory will be your optimized images.
